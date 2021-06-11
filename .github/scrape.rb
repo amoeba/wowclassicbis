bf95fdfbebebeaee1dbb7852b2d48a887eae9761
Bryce <petridish@gmail.com>
2021-06-10 23:22:40 -0800

    Make scrape.rb handle multiple sheet ids

--- .github/scrape.rb
+++ .github/scrape.rb
@@ -9,8 +9,13 @@ DECODED_IO = StringIO.new(DECODED)
 SESSION = GoogleDrive::Session.from_service_account_key(
   DECODED_IO)
 
-file = SESSION.file_by_url(ENV["SHEET_URL"])
+# "mysheetid1,mysheetid2"
+SHEET_IDS = ENV["SHEET_IDS"].split(",")
 
-file.worksheets.each do |ws|
-  ws.export_as_file("#{ws.title}.csv")
+SHEET_IDS.each do |id|
+  file = SESSION.file_by_id(id)
+
+  file.worksheets.each do |ws|
+    ws.export_as_file("#{ws.title}.csv")
+  end
 end
