36b8deec6dcd27c53b0d622236e53408225d3ce6
Bryce <petridish@gmail.com>
2021-06-11 09:02:07 -0800

    Make scrape script more tolerant

--- .github/scrape.rb
+++ .github/scrape.rb
@@ -14,8 +14,15 @@ SHEET_IDS = ENV["SHEET_IDS"].split(",")
 
 SHEET_IDS.each do |id|
   file = SESSION.file_by_id(id)
+  puts file.title
 
   file.worksheets.each do |ws|
-    ws.export_as_file("#{ws.title}.csv")
+    puts ws.title
+
+    begin
+      ws.export_as_file("#{ws.title}.csv")
+    rescue
+      puts "FAILED"
+    end
   end
 end
