00a69fc994f45bc0e6dbb69d11719ebf607b5e59
Bryce <petridish@gmail.com>
2021-06-10 22:12:10 -0800

    Scrape a single file into multiple CSVs

--- .github/scrape.rb
+++ .github/scrape.rb
@@ -9,6 +9,8 @@ DECODED_IO = StringIO.new(DECODED)
 SESSION = GoogleDrive::Session.from_service_account_key(
   DECODED_IO)
 
-SESSION.files.each do |file|
-  file.export_as_file("#{file.title}.csv", "text/csv")
+file = SESSION.file_by_url(ENV["SHEET_URL"])
+
+file.worksheets.each do |ws|
+  ws.export_as_file("#{ws.title}.csv")
 end
